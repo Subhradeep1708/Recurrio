b27ede8b4d6e49ac87042ee258fd9752de2fcc7b
Subhradeep Sardar <subhradeep1708@gmail.com>
2026-08-04 18:28:29 +0530

    Add authentication and onboarding screens with layout updates

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,33 @@
+import "@/global.css";
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-background">
+      <Text className="text-xl font-bold text-success">
+        Welcome to Nativewind!
+      </Text>
+      <Link href="/onboarding" className="mt-4 rounded bg-primary text-white p-4">GO</Link>
+      <Link href="/(auth)/sign-in" className="mt-4 rounded bg-primary text-white p-4">GO Signin</Link>
+      <Link href="/(auth)/sign-up" className="mt-4 rounded bg-primary text-white p-4">GO Signup</Link>
+      <Link href="/(auth)/sign-up" className="mt-4 rounded bg-primary text-white p-4">GO Signup</Link>
+
+
+
+      <Link href={{
+        pathname: "/subscriptions/[id]",
+        params: { id: "spotify" }
+      }}
+        className="mt-4 rounded bg-primary text-white p-4">Spotify</Link>
+      <Link href={{
+        pathname: "/subscriptions/[id]",
+        params: { id: "claude" }
+      }}
+        className="mt-4 rounded bg-primary text-white p-4">Claude</Link>
+
+
+
+    </View>
+  );
+}
---
+++ app/onboarding.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const onboarding = () => {
+  return (
+      <View className="flex-1 items-center justify-center bg-background">
+      <Text>onboarding ho gaya bencho</Text>
+    </View>
+  )
+}
+
+export default onboarding
