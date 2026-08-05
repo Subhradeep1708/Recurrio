6e605abdd0e5a0663b2370a6645ae11dad65ea83
Subhradeep Sardar <subhradeep1708@gmail.com>
2026-08-05 15:23:30 +0530

    feat: Add subscription management components and enhance home screen layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -11,13 +11,13 @@ const tabBar = components.tabBar;
 const TabLayout = () => {
 
     const insets = useSafeAreaInsets(); // this gives the hardware default spacing taken by default
-    console.log(insets);
 
     const TabIcon = ({ focused, icon }: TabIconProps) => {
         return (
             <View className="tabs-icon">
                 <View className={clsx('tabs-pill', focused && 'tabs-active')}>
                     <Image source={icon} resizeMode="contain" className="tabs-glyph" />
+                    {/* <Text className={clsx('tabs-label', focused && 'tabs-active-label')}>{focused ? 'Active' : 'Inactive'}</Text> */}
                 </View>
             </View>
         )
